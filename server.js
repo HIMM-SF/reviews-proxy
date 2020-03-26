a37ff7b97fa4dd344b5287bf9a7c6e1c937c8b64
minhoCha <minhocha1004@gmail.com>
2020-03-25 20:33:13 -0700

    proxy updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3040;
+const port = 9000;
 const path = require("path");
 const cors = require("cors");
 const httpProxy = require("http-proxy");
@@ -10,23 +10,28 @@ const proxy = httpProxy.createProxyServer({});
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('/api/reviews', (req, res) => {
-    proxy.web(req, res, {
-        target: "http://localhost:4000"
-      });
-} )
+// app.all('/api/images', (req, res) => {
+//   proxy.web(req, res, {
+//       target: "http://localhost:8081"
+//     });
+// } )
 
-// app.all('/recommendations', (req, res) => {
+// app.all('/api/rooms', (req, res) => {
 //     proxy.web(req, res, {
-//         target: "http://localhost:3009"
+//         target: "http://localhost:4000"
 //       });
 // } )
 
-// app.all('/photos', (req, res) => {
-//     proxy.web(req, res, {
-//         target: "http://localhost:3001"
-//       });
+// app.all('/api/rooms', (req, res) => {
+//   proxy.web(req, res, {
+//       target: "http://localhost:8080"
+//     });
 // } )
 
+// app.all('/api/reviews', (req, res) => {
+//     proxy.web(req, res, {
+//         target: "http://localhost:3002"
+//       });
+// } )
 
 app.listen(port, () => console.log(`Proxy server running on port ${port}`));
